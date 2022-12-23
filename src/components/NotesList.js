553cfcb2d548f6c48c5327ffc81ea4c03c77df4a
maemay85 <hellomaemay@gmail.com>
2022-12-23 18:16:29 -0500

    notes show up as buttons, SoundPlay function is making a GET request - will need to import audio somehow?

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { Howl, Howler } from 'howler';
 
 
 const NotesList = ({ notes }) => {
+
+  const SoundPlay = (src) => {
+   const sound = new Howl({
+      src
+    })
+    Howler.volume(.5)
+    sound.play()
+  }
+
+
   console.log('NotesList:', notes)
   return (
     <div id="noteslist" className="column">
-
-       {notes.map(note =>
-        <div key={note.id}>
-          {note.name}
-        </div>)}
+       {notes.map((note, index) => {
+      return (
+        <button key={index} onClick={() => SoundPlay(note.audioUrl)}>{note.name}</button>
+      )
+    })}
     </div>
   );
 };
